f68ea0792e7cfbb157b99308226204b1698de2b9
didi <lijundi@bupt.edu.cn>
2019-05-21 16:54:20 +0800

    视图函数、各个页面已建立，待完善

--- server/urls.py
+++ server/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('reception/', views.reception, name='reception'),
+    path('get_records/', views.get_records, name='get_records'),
+    path('get_invoice/', views.get_invoice, name='get_invoice'),
+    path('manager/', views.manager, name='manager'),
+    path('manager_set_para/', views.manager_set_para, name='manager_set_para'),
+    path('manager_check_state/', views.manager_check_state, name='manager_check_state'),
+    path('boss/', views.boss, name='boss'),
 ]
--- server/views.py
+++ server/views.py
@@ -1,6 +1,49 @@
 from django.shortcuts import render
+from .models import *
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', locals())
+    return render(request, 'index.html')
+
+
+# 前台
+def reception(request):
+    return render(request, 'reception.html')
+
+
+def get_records(request):
+    if request.method == 'post':
+        room_id = request.POST['room_id']
+        records = RequestDetailRecords.objects.filter(room_id=room_id).order_by('request_time')
+        return render(request, 'reception.html', {'records': records})
+    else:
+        return render(request, 'reception.html')
+
+
+def get_invoice(request):
+    if request.method == 'post':
+        room_id = request.POST['room_id']
+        room = Room.objects.get(room_id=room_id)
+    return render(request, 'reception.html', locals())
+
+
+# 管理员
+def manager(request):
+    return render(request, 'manager.html', locals())
+
+
+def manager_set_para(request):
+    return render(request, 'manager_set_para.html', locals())
+
+
+def manager_check_state(request):
+    return render(request, 'manager_check_state.html', locals())
+
+
+# 经理
+def boss(request):
+    return render(request, 'boss.html', locals())
+
+
+
